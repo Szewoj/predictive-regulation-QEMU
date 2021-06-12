6d2c37b37fc3e513abd5f4495b077fd69a371a27
Szewoj <wojciech_szerszen@op.pl>
2021-06-12 14:40:24 +0200

    [add] Access luminance on webcam

--- Simulation/simulation.cpp
+++ Simulation/simulation.cpp
@@ -1,18 +1,19 @@
+#include <fcntl.h>
 #include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/videoio.hpp>
-#include <stdio.h>
-#include <vector>
+//#include <semaphore.h>
+//#include <sys/mman.h>
+//#include <sys/stat.h>
+
 using namespace cv;
 using namespace std;
+
 int main(int, char **) {
-  vector<uchar> buffer;
-  vector<int> encodeParams;
-  encodeParams.push_back(IMWRITE_JPEG_QUALITY);
-  encodeParams.push_back(10);
+
   Mat frame;
-  Mat decodedFrame;
+
   //--- INITIALIZE VIDEOCAPTURE
   VideoCapture cap;
   // open the default camera using default API
@@ -27,29 +28,48 @@ int main(int, char **) {
     cerr << "ERROR! Unable to open camera\n";
     return -1;
   }
-  //--- GRAB AND WRITE LOOP
-  cout << "Start grabbing" << endl << "Press any key to terminate" << endl;
-  for (;;) {
-    buffer.clear();
-    // wait for a new frame from camera and store it into 'frame'
-    cap.read(frame);
-    // check if we succeeded
-    if (frame.empty()) {
-      cerr << "ERROR! blank frame grabbed\n";
-      break;
-    }
-    imencode(".jpg", frame, buffer, encodeParams);
 
-    decodedFrame = imdecode(buffer, IMREAD_UNCHANGED);
-    if (decodedFrame.empty()) {
-      cerr << "ERROR! cannot decode frame\n";
-      break;
-    }
-    // show live and wait for a key with timeout long enough to show images
-    imshow("Live", decodedFrame);
-    if (waitKey(5) == 27)
+  //--- GRAB AND WRITE LOOP
+  cout << "Start grabbing" << '\n' << "Press any key to terminate" << '\n';
+
+  // wait for a new frame from camera and store it into 'frame'
+  cap.read(frame);
+  // check if we succeeded
+  if (frame.empty()) {
+    cerr << "ERROR! blank frame grabbed\n";
+    return 1;
+  }
+
+  // show live and wait for a key with timeout long enough to show images
+  imshow("Snapshot", frame);
+
+  //int centerRow = frame.rows / 2;
+  //int centerCol = frame.cols / 2;
+
+  int dims = frame.size.dims();
+
+  cout << "Matrix dimensions: " << dims << '\n';
+  for (int i = 0; i < dims; ++i){
+    cout << frame.size[i] << ' ';
+  }
+  cout << '\n';
+
+  int centerRow = frame.rows / 2;
+  int centerCol = frame.cols / 2;
+
+  cout << "Center row, col " << centerRow << ' ' << centerCol << '\n';
+
+  Vec3b pixel = frame.at<Vec3b>(centerRow, centerCol);
+
+  int luminance = 0.299*pixel[2] + 0.587*pixel[1] + 0.114*pixel[0];
+
+  cout << "Luminance " << luminance << '\n';
+
+  for (;;) {
+    if (waitKey(100) == 27)
       break;
   }
+
   // the camera will be deinitialized automatically in VideoCapture destructor
   return 0;
 }
