ec28197efb634bb5b79df818b01b916eded5e9f7
Szewoj <wojciech_szerszen@op.pl>
2021-06-13 20:22:36 +0200

    [add] DMC library

---
+++ Regulator/DMC.py
@@ -0,0 +1,19 @@
+import StepResponse as SR
+import numpy as np
+
+class DMC:
+    
+    U_MIN = 0.0
+    U_MAX = 
+
+    def __init__(self):
+        self._D = 0
+        self._Dz = 0
+
+        self._deltaUp = 0
+        self._deltaZp = 0
+
+        self._Ku = 0
+        self._Kz = 0
+
+    
--- Regulator/qemu-regulator.py
+++ Regulator/qemu-regulator.py
@@ -1,1 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+import StepResponse as SR
+
+if __name__ == '__main__':
+    print("Hello World!")
+    print(SR.Su[10])
